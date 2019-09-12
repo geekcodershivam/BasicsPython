a30be17a7c191957980a4d0122467f45049d7f86
Shivammani19 <shivamtripathi214@gmail.com>
2019-09-12 23:06:17 +0530

    Linkedlist

--- Chapter1.py
+++ Chapter1.py
@@ -23,3 +23,32 @@ print (name[0:0])
 print (name[1:9])
 print (name[:10])
 
+class Node:
+    def __int__(self, value):
+        self.value=value
+        self.next=None
+class LinkedList:
+    def __int__(self):
+            self.start=None
+
+            def insert(self,value):
+                if self.start==None:
+                    self.start=Node(value)
+                    current=self.start
+                    while current.next is not None:
+                        current=current.next
+                        current.next=Node(value)
+
+                      def print(self):
+                     current = self.start
+                      while current is not None:
+                        print(current.value)
+                         current=current.next
+
+linkedlist = LinkedList()
+linkedlist.insert(10)
+linkedlist.insert(20)
+linkedlist.insert(30)
+linkedlist.insert(40)
+linkedlist.insert(50)
+linkedlist.print()
